d0e866061e8d30cb54102a8e7bfb9e0131497730
sjp1123 <shijun2009@naver.com>
2023-06-03 20:59:30 +0900

    chat

--- src/server.js
+++ src/server.js
@@ -1,15 +1,62 @@
 import http from "http";
-import WebSocket from "ws";
+import SocketIO from "socket.io";
 import express from "express";
+
 const app = express();
 app.set("view engine", "pug");
 app.set("views", __dirname + "/views");
 app.use("/public", express.static(__dirname + "/public"));
+app.use("/views", express.static(__dirname + "/views"));
 app.get("/", (_, res) => res.render("home"));
 app.get("/*", (_, res) => res.redirect("/"));
-const handleListen = () => console.log(`Listening on http://localhost:3000`);
-const server = http.createServer(app);
-const wss = new WebSocket.Server({ server });
+
+const httpServer = http.createServer(app);
+const wsServer = SocketIO(httpServer);
+
+function publicRooms() {
+    const {
+        sockets: {
+            adapter: { sids, rooms },
+        },
+    } = wsServer;
+    const publicRooms = [];
+    rooms.forEach((_, key) => {
+        if (sids.get(key) === undefined) {
+            publicRooms.push(key);
+        }
+    });
+    return publicRooms;
+}
+
+function countRoom(roomName) {
+    return wsServer.sockets.adapter.rooms.get(roomName)?.size;
+}
+
+wsServer.on("connection", (socket) => {
+    socket["nickname"] = "익명";
+    wsServer.sockets.emit("room_change", publicRooms());
+    socket.on("enter_room", (roomName, done) => {
+        socket.join(roomName);
+        done(countRoom(roomName));
+        socket.to(roomName).emit("welcome", socket.nickname, countRoom(roomName));
+        wsServer.sockets.emit("room_change", publicRooms());
+    });
+    socket.on("disconnecting", () => {
+        socket.rooms.forEach((room) =>
+            socket.to(room).emit("bye", socket.nickname, countRoom(room)-1)
+        );
+    });
+    socket.on("disconnect", () => {
+        wsServer.sockets.emit("room_change", publicRooms());
+    });
+    socket.on("new_message", (msg, room, done) => {
+        socket.to(room).emit("new_message", `${socket.nickname} : ${msg}`);
+        done();
+    });
+    socket.on("nickname", (nickname) => (socket["nickname"] = nickname));
+});
+
+/*
 const sockets = [];
 
 wss.on("connection", (socket) => {
@@ -22,13 +69,14 @@ wss.on("connection", (socket) => {
         switch (message.type) {
             case "new_message":
                 sockets.forEach((aSocket) =>
-                    aSocket.send(`${socket.nickname}: ${message.payload}`)
+                aSocket.send(`${socket.nickname}: ${message.payload}`)
                 );
                 break;
-            case "nickname":
-                socket["nickname"] = message.payload;
-        }
-    });
-});
-
-server.listen(1123, handleListen);
+                case "nickname":
+                    socket["nickname"] = message.payload;
+                }
+            });
+        });
+        */
+const handleListen = () => console.log(`Listening on http://localhost:1123`);
+httpServer.listen(1123, handleListen);
